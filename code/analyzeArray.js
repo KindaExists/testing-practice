11729adf72d40cccf8b4fc55e8ba78ef1a0717d9
KindaExists <ehrencastillo@gmail.com>
2021-12-06 14:19:49 +0800

    create analyzeArray code

---
+++ code/analyzeArray.js
@@ -0,0 +1,12 @@
+function analyzeArray(arr) {
+  return {
+    average: arr.reduce((sum, value) => {
+      return sum + value;
+    }, 0) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length
+  };
+};
+
+module.exports = analyzeArray;
