78793e8c7e27fbe0246341b3ee0c07f3c1a48cc6
KindaExists <ehrencastillo@gmail.com>
2021-12-06 14:09:41 +0800

    seperate caesarCipher functions

--- code/caesarCipher.js
+++ code/caesarCipher.js
@@ -1,21 +1,27 @@
+function shiftCharCode(charCode, shift, minCode, maxCode) {
+  const range = (maxCode - minCode) + 1;
 
+  const base = charCode - minCode;
+  const shifted = (((base + shift) % range) + range) % range;
+  return shifted + minCode;
+}
 
 function caesarCipher(string, shift) {
   const chars = string.split('');
   const shifted = chars.map(char => {
-    const charValue = char.charCodeAt();
+    const charCode = char.charCodeAt();
 
     // Note: Javascript's modulo does not work correctly with negative inputs
     // This can be fixed by using the following formula: [ ((input % n) + n) % n ]
-    if (charValue >= 48 && charValue <= 57) {
+    if (charCode >= 48 && charCode <= 57) {
       // Numerical Character
-      return String.fromCharCode((((((charValue - 48) + shift) % 10) + 10) % 10) + 48);
-    } else if (charValue >= 65 && charValue <= 90) {
+      return String.fromCharCode(shiftCharCode(charCode, shift, 48, 57));
+    } else if (charCode >= 65 && charCode <= 90) {
       // Upper Case
-      return String.fromCharCode((((((charValue - 65) + shift) % 26) + 26) % 26) + 65);
-    } else if (charValue >= 97 && charValue <= 122) {
+      return String.fromCharCode(shiftCharCode(charCode, shift, 65, 90));
+    } else if (charCode >= 97 && charCode <= 122) {
       // Lower Case
-      return String.fromCharCode((((((charValue - 97) + shift) % 26) + 26) % 26) + 97);
+      return String.fromCharCode(shiftCharCode(charCode, shift, 97, 122));
     }
 
     return char;
