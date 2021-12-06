f43eecedcfb5f0fa077060fac40846c8ca2f0b83
KindaExists <ehrencastillo@gmail.com>
2021-12-06 13:57:01 +0800

    add tests for caesarCipher

---
+++ tests/caesarCipher.test.js
@@ -0,0 +1,25 @@
+const caesarCipher = require('../code/caesarCipher');
+
+test('Can shift by 3', () => {
+  expect(caesarCipher('example', 3)).toEqual('hadpsoh');
+});
+
+test('Can shift by -2', () => {
+  expect(caesarCipher('example', -2)).toEqual('cvyknjc');
+});
+
+test('Wraps forwards from z to a', () => {
+  expect(caesarCipher('xyz', 3)).toEqual('abc');
+});
+
+test('Wraps backwards from a to z', () => {
+  expect(caesarCipher('cba', -3)).toEqual('zyx');
+});
+
+test('Retains casing', () => {
+  expect(caesarCipher('Case', 2)).toEqual('Ecug');
+});
+
+test('Ignores punctuation', () => {
+  expect(caesarCipher('.hello!', 3)).toEqual('.khoor!');
+});
